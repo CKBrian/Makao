f8d6df171106c9722f83286e11202124ee7827ba
Brian <chumobrian@gmail.com>
2024-02-26 13:28:37 +0300

    feat(BAseModel):Add a BaseModel class

--- makao_site/basemodel/models.py
+++ makao_site/basemodel/models.py
@@ -1,3 +1,12 @@
+"""Defines a module that implements a BaseModel class"""
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """Defines the BaseModel class"""
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """"""
+        abstract = True
