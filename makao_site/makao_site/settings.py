7ca50d83011d2b5cef8dc2d251c62dfdf32cdc1d
Brian <chumobrian@gmail.com>
2024-02-27 21:54:44 +0300

    Feat: Adjust Installed apps in settings.py for properties, amenities, places and user apps

--- makao_site/makao_site/settings.py
+++ makao_site/makao_site/settings.py
@@ -22,6 +22,9 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 	'base_model.apps.BaseModelConfig',
     'users',
+    'cities',
+    'places',
+    'properties',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
