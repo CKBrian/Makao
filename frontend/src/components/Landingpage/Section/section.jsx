3c589044efecde1a68e4caeb1a357778a6fcee21
Brian <chumobrian@gmail.com>
2024-03-15 05:03:12 +0300

    feature(Homepage):Add Landing page

--- frontend/src/components/Landingpage/Section/section.jsx
+++ frontend/src/components/Landingpage/Section/section.jsx
@@ -1,21 +1,19 @@
 import './section.css';
-import searchIcon from '../../../assets/Icons/search.svg';
 
 
 function Section () {
 
+	const icon = "https://icons.getbootstrap.com/assets/icons/search.svg"
+
 	return (
 		<>
             <section className="home-main-section">
-			<div className="img-wrapper">
-                    <div className="property-image"></div>
-                </div>
                 <div className="call-to-action">
                     <span className="title">Looking For a House?</span>
                 </div>
 				<div className="search-bar subtitle">
 					<form>
-					<img src={searchIcon} alt="Search Icon" />
+					<img src={icon} alt="Search Icon" className="btn" />
 					<input
 						type='text'
 						placeholder='Search Properties by Name, Location...'
