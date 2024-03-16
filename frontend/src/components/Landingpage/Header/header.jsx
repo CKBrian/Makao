606615018ab9376c2e85e331837a4e136d77528b
Robert Odhiambo <odhizrober254@gmail.com>
2024-03-16 13:44:07 +0300

    feat: landing page images and section

--- frontend/src/components/Landingpage/Header/header.jsx
+++ frontend/src/components/Landingpage/Header/header.jsx
@@ -1,35 +1,39 @@
 import './header.css';
+import React, { useState } from 'react';
 
-function Header () {
+function MobileMenu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <>
-      <div className="main-header">
-        <a href="/" className="brand-logo">
-          <div className="brand-logo-name">Makao</div>
-        </a>
-        <nav className="main-nav">
-          <ul >
-            <li ><a href="" >About</a></li>
-            <li ><a href="" >Rent</a></li>
-            <li ><a href="" >Community</a></li>
-            <li ><a href="" >Advertise</a></li>
-          </ul>
-        </nav>
-        <nav className="login-nav">
-          <ul >
-            <li ><a href="" >Login</a></li>
-            <li >
-              <a href="" >
-              <button className="btn">Sign up</button>
-              </a>
-            </li>
-          </ul >
-        </nav>
-      </div>
-    </>
-  )
+    <div className="main-header">
+      <a href="/" className="brand-logo">
+        <div className="brand-logo-name">Makao</div>
+      </a>
+      <nav className={`main-nav ${isMenuOpen ? 'open' : ''}`}>
+        <ul className="mobile-nav">
+          <li><a href="">About</a></li>
+          <li><a href="">Rent</a></li>
+          <li><a href="">Community</a></li>
+          <li><a href="">Advertise</a></li>
+        </ul>
+      </nav>
+      <nav className="login-nav">
+        <ul>
+          <li><a className=''>Login</a></li>
+          <li>
+            <button className="btn">Sign up</button>
+          </li>
+        </ul>
+      </nav>
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-list mobile-nav-icon" viewBox="0 0 16 16" onClick={toggleMenu}>
+        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+      </svg>
+    </div>
+  );
 }
 
-export default Header
-
+export default MobileMenu;
