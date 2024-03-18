c68e309dab3522ae3e5a50350cff2008117f688b
Brian <chumobrian@gmail.com>
2024-03-18 18:10:13 +0300

    feat(Advertise):Add advertise property

--- frontend/src/components/Properties/PropertyList.jsx
+++ frontend/src/components/Properties/PropertyList.jsx
@@ -1,11 +1,13 @@
 import Header from '../Shared/Header/header'
 import Footer from '../Shared/Footer/footer'
+import Section from './section'
 
 
 const PropListings = () => {
     return ( 
         <>
             <Header></Header>
+            <Section/>
             <Footer></Footer>
         </>
      );
